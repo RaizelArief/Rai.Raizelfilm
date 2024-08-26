a38ebc24772719b8c07a91f61bfa071639576e27
RaizelArief <dianuve@gmail.com>
2024-08-26 16:56:37 +0700

    hapus homepage

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import "bootstrap/dist/css/bootstrap.min.css";
-import HomePage from './HomePage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
